e56b074d45d4b98ea558347408dfdec8c77a240a
Peter Zhang <127898539+ZMPeterZhang@users.noreply.github.com>
2024-10-26 18:25:55 -0400

    add empty struct utility, move data to the namespace they belong under Shared

---
+++ PluginRhino/Utilities/GraphStrutUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphHop.Shared.Data;
+
+namespace GraphHop.PluginRhino.Utilities
+{
+    public static class GraphStrutUtility
+    {
+
+    }
+}
