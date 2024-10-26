4c2cc886761a3a8dc3f2d987f8c7848fd7188309
Cullen Sarles <cullen.sarles@ryancompanies.com>
2024-10-26 16:09:39 -0500

    remote copilot halucination

--- PluginRhino/Utilities/GHDigestUtility.cs
+++ PluginRhino/Utilities/GHDigestUtility.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Grasshopper.Rhinoceros.Display.Params.Param_ModelView;
 
 namespace PluginTemplate.PluginRhino.Utilities
 {
