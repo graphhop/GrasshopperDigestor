2cf7040cb5b3bda215937cdf5948f50b2258ed3d
Alex Schiftner <alex@shapediver.com>
2024-10-26 15:32:19 -0400

    struct properties - initial version

--- SharedRhino/Data/DataOutputNode.cs
+++ SharedRhino/Data/DataOutputNode.cs
@@ -1,6 +1,18 @@
-﻿namespace GraphHop.SharedRhino.Data;
+﻿using System;
+
+namespace GraphHop.SharedRhino.Data;
 
 public struct DataOutputNode
 {
-    
+    /// <summary>
+    /// https://developer.rhino3d.com/api/grasshopper/html/P_Grasshopper_Kernel_GH_InstanceDescription_InstanceGuid.htm
+    /// </summary>
+    public Guid InstanceGuid;
+
+    /// <summary>
+    /// https://developer.rhino3d.com/api/grasshopper/html/P_Grasshopper_Kernel_GH_InstanceDescription_NickName.htm
+    /// </summary>
+    public string NickName;
+
+    // TODO: Reverse, DataMapping, etc
 }
--- SharedRhino/Data/DocumentNode.cs
+++ SharedRhino/Data/DocumentNode.cs
@@ -1,6 +1,12 @@
-﻿namespace GraphHop.SharedRhino.Data;
+﻿using System;
+
+namespace GraphHop.SharedRhino.Data;
 
 public struct DocumentNode
 {
-    
+    /// <summary>
+    /// https://developer.rhino3d.com/api/grasshopper/html/P_Grasshopper_Kernel_GH_Document_DocumentID.htm
+    /// </summary>
+    public Guid DocumentID;
+
 }
