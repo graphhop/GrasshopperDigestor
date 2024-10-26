a4bdc988e1664bf4dab7ccaea6300b5018391374
Peter Zhang <127898539+ZMPeterZhang@users.noreply.github.com>
2024-10-26 17:47:09 -0400

    print object metadata

--- PluginRhino/Utilities/GHDigestUtility.cs
+++ PluginRhino/Utilities/GHDigestUtility.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Rhino.Geometry;
 
 namespace PluginTemplate.PluginRhino.Utilities
 {
@@ -58,40 +59,74 @@ namespace PluginTemplate.PluginRhino.Utilities
             }
             else
             {
-                System.Diagnostics.Debug.WriteLine("The object is neither a parameter nor a component.");
+                Debug.WriteLine("The object is neither a parameter nor a component.");
             }
         }
 
         public static void IterateDocumentObjects(GH_Document ghDocument)
         {
-            System.Diagnostics.Debug.WriteLine("");
-            PrintDocumentProperties(ghDocument);
-            // Iterate through all document objects and print their names
-            foreach (IGH_DocumentObject obj in ghDocument.Objects)
+            try
             {
                 System.Diagnostics.Debug.WriteLine("");
-                System.Diagnostics.Debug.WriteLine($"Object Name: {obj.Name}, NickName:  {obj.NickName} ");
-                System.Diagnostics.Debug.WriteLine("----------Input&Outputs---------");
-                GetConnectedObjects(obj);
+                PrintDocumentProperties(ghDocument);
+                // Iterate through all document objects and print their names
+                foreach (IGH_DocumentObject obj in ghDocument.Objects)
+                {
+                    Debug.WriteLine("");
+                    PrintDocumentObjectProperties(obj);
+                    Debug.WriteLine($"Object Name: {obj.Name}, NickName:  {obj.NickName} ");
+                    Debug.WriteLine("----------Input&Outputs---------");
+                    GetConnectedObjects(obj);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
 
         public static void PrintDocumentProperties(GH_Document ghDocument)
         {
             Debug.WriteLine($"Author: {ghDocument.Author}");
-            Debug.WriteLine($"DisplayName: {ghDocument.DisplayName}");
+            Debug.WriteLine($"DisplayName: {ghDocument.DisplayName ?? "None"}");
             Debug.WriteLine($"DocumentID: {ghDocument.DocumentID}");
-            Debug.WriteLine($"FilePath: {ghDocument.FilePath}");
+            Debug.WriteLine($"FilePath: {ghDocument.FilePath ?? "None"}");
             Debug.WriteLine($"Owner: {ghDocument.Owner}");
             Debug.WriteLine($"RuntimeID: {ghDocument.RuntimeID}");
             Debug.WriteLine($"SolutionState: {ghDocument.SolutionState}");
+        }
+
+        public static void PrintDocumentObjectProperties(IGH_DocumentObject obj)
+        {
+            Debug.WriteLine("----------Component Metadata---------");
+            //Debug.WriteLine($"Attributes: {obj.Attributes}");
+            Debug.WriteLine($"Category: {obj.Category ?? "None"}");
+            Debug.WriteLine($"ComponentGuid: {obj.ComponentGuid}");
+            Debug.WriteLine($"Description: {obj.Description ?? "None"}");
+            Debug.WriteLine($"Exposure: {obj.Exposure}");
+            Debug.WriteLine($"HasCategory: {obj.HasCategory}");
+            Debug.WriteLine($"HasSubCategory: {obj.HasSubCategory}");
+            Debug.WriteLine($"IconDisplayMode: {obj.IconDisplayMode}");
+            Debug.WriteLine($"InstanceDescription: {obj.InstanceDescription ?? "None"}");
+            Debug.WriteLine($"InstanceGuid: {obj.InstanceGuid}");
+            Debug.WriteLine($"Keywords: {JoinKeywords(obj.Keywords)}");
+            Debug.WriteLine($"Name: {obj.Name ?? "None"}");
+            Debug.WriteLine($"NickName: {obj.NickName ?? "None"}");
+            Debug.WriteLine($"Obsolete: {obj.Obsolete}");
+            Debug.WriteLine($"SubCategory: {obj.SubCategory ?? "None"}");
+        }
+
+        private static string JoinKeywords(IEnumerable<string> keywords)
+        {
+            return keywords != null ? string.Join(", ", keywords) : "None";
         }
 
         private static void ProcessSource(IGH_Param source)
         {
             // Example processing logic for sources
             string parentComponentName = source.Attributes?.GetTopLevel?.DocObject?.Name ?? "Unknown";
-            System.Diagnostics.Debug.WriteLine($"Input Type: {source.GetType().Name}, NickName: {source.NickName}, Parent Component: {parentComponentName}");
+            Debug.WriteLine($"Input Type: {source.GetType().Name}, NickName: {source.NickName}, Parent Component: {parentComponentName}");
             // Add additional processing logic here
         }
 
@@ -99,7 +134,7 @@ namespace PluginTemplate.PluginRhino.Utilities
         {
             // Example processing logic for recipients
             string parentComponentName = recipient.Attributes?.GetTopLevel?.DocObject?.Name ?? "Unknown";
-            System.Diagnostics.Debug.WriteLine($"Output Type: {recipient.GetType().Name}, NickName: {recipient.NickName}, Parent Component: {parentComponentName}");
+            Debug.WriteLine($"Output Type: {recipient.GetType().Name}, NickName: {recipient.NickName}, Parent Component: {parentComponentName}");
             // Add additional processing logic here
         }
     }
